cf640ec1f337f852c71df62ce68cd7a1c3c3faa0
alexander-matsievsky <alexander.matsievsky@gmx.com>
2016-10-23 21:26:14 +0300

    feature #199: Functions and Fractals: Sierpinski triangles

--- All_Domains/Functional_Programming/Recursion/test/Spec.hs
+++ All_Domains/Functional_Programming/Recursion/test/Spec.hs
@@ -1,9 +1,11 @@
 import           FunctionalProgrammingWarmupsInRecursionFibonacciNumbers
 import           FunctionalProgrammingWarmupsInRecursionGcd
+import           FunctionsAndFractalsSierpinskiTriangles
 import           PascalsTriangle
 
 main = do
     _ <- FunctionalProgrammingWarmupsInRecursionGcd.runTests
     _ <- FunctionalProgrammingWarmupsInRecursionFibonacciNumbers.runTests
     _ <- PascalsTriangle.runTests
+    _ <- FunctionsAndFractalsSierpinskiTriangles.runTests
     return ()
